a8b9fee8bd40a44c2a64286da6915fc546cded30
established001 <blaccvibes@gmail.com>
2022-09-02 15:29:02 -0700

    started working on the desktop version

--- script.js
+++ script.js
@@ -8,12 +8,5 @@ setInterval(function(){
 }, 2000);
 
 
-let counterTwo = 1;
-setInterval(function(){
-    document.getElementById("radio" + counterTwo).checked = true;
-    counterTwo++;
-    if (counterTwo > 4) {
-        counterTwo = 1
-    }
-}, 2000);
+
 
