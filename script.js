2eb32bda8357f3eb585cf12fb64d87b985bbf8ea
established001 <blaccvibes@gmail.com>
2022-09-02 21:15:25 -0700

    still working on desktop

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ setInterval(function(){
     }
 }, 2000);
 
+const writeMessage = function message() {
+    const say = alert('WELCOME TO VILLAMAR😊, PLEASE DO WELL TO ENJOY YOUR STAY');
+    const askUser = prompt('Have you ever visited Villamar before?')
+    if (askUser === 'yes' || 'Yes' || 'YES') {
+        alert('GOOD TO HAVE YOU HERE. PLEASE ENJOY YOUR STAY')
+    } else if(askUser === 'no' || 'No' || 'NO') {
+        alert('AWESOME!, WELCOME BACK')
+    } else {
+        alert('INVALID COMMAND')
+    }
+}
 
 
-
+const total = writeMessage()
